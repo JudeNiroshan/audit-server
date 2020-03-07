ac7c49ec7b8b0284619f4a32c8cb978fa1116372
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2020-03-07 14:43:38 +0100

    remove unused instance variable

--- src/main/java/com/logger/rest/auditserver/EventService.java
+++ src/main/java/com/logger/rest/auditserver/EventService.java
@@ -1,6 +1,5 @@
 package com.logger.rest.auditserver;
 
-import com.logger.rest.auditserver.mediator.Mediator;
 import com.logger.rest.auditserver.mediator.MediatorFactory;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -8,14 +7,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class EventService {
 
-    private final MediatorFactory mediatorFactory;
-
-    public EventService(MediatorFactory mediatorFactory) {
-        this.mediatorFactory = mediatorFactory;
-    }
-
     public String handleEvent(@NonNull String eventJson) {
-        Mediator grpcMediator = MediatorFactory.getMediator("grpc");
-        return grpcMediator.callRpc(eventJson);
+        return MediatorFactory.getMediator("grpc")
+                .callRpc(eventJson);
     }
 }
--- src/main/java/com/logger/rest/auditserver/mediator/Mediator.java
+++ src/main/java/com/logger/rest/auditserver/mediator/Mediator.java
@@ -1,6 +1,16 @@
 package com.logger.rest.auditserver.mediator;
 
 public interface Mediator {
+    /**
+     * Unique identifier string for Mediator implementation.
+     * @return
+     */
     String getType();
+
+    /**
+     * call the rpc method and return result
+     * @param dataAsJsonString
+     * @return
+     */
     String callRpc(String dataAsJsonString);
 }
