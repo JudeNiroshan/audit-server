4505113af5c48e0e406ffc291627cf3f742423bc
Jude Niroshan <jude.niroshan11@gmail.com>
2020-03-08 21:55:17 +0100

    service discovery with eureka (#9)
    service discovery with eureka

--- src/test/java/com/logger/rest/auditserver/AuditServerApplicationTests.java
+++ src/test/java/com/logger/rest/auditserver/AuditServerApplicationTests.java
@@ -1,9 +1,10 @@
 package com.logger.rest.auditserver;
 
+import com.logger.rest.auditserver.config.MyTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {MyTestConfiguration.class})
 class AuditServerApplicationTests {
 
     @Test
