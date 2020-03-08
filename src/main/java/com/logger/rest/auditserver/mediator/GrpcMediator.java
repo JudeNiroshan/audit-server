6c4642541045fd4dcabbb11a2a5d6b5529ac50f7
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2020-03-08 20:30:39 +0100

    service discovery with eureka

--- src/main/java/com/logger/rest/auditserver/mediator/GrpcMediator.java
+++ src/main/java/com/logger/rest/auditserver/mediator/GrpcMediator.java
@@ -2,22 +2,43 @@ package com.logger.rest.auditserver.mediator;
 
 import com.logger.rest.auditserver.EventServiceGrpc;
 import com.logger.rest.auditserver.Events;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 public class GrpcMediator implements Mediator {
 
+    private final EurekaClient eurekaClient;
+    private final LoadBalancerClient loadBalancerClient;
     private final String target = "127.0.0.1:50051";
     private EventServiceGrpc.EventServiceBlockingStub eventServiceBlockingStub;
 
+    public GrpcMediator(@Qualifier("eurekaClient") EurekaClient eurekaClient, LoadBalancerClient loadBalancerClient) {
+        this.eurekaClient = eurekaClient;
+        this.loadBalancerClient = loadBalancerClient;
+    }
+
     @PostConstruct
     private void initCommunicationChannel() {
-        ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+//        InstanceInfo nextServerInfo = DiscoveryManager.getInstance()
+//                .getDiscoveryClient()
+//                .getNextServerFromEureka("127.0.0.1", false);
+
+//        ServiceInstance serviceInstance=loadBalancerClient.choose("logger-app"); // mehema karanam sira!
+//        URI uri = serviceInstance.getUri();
+        List<InstanceInfo> instanceInfoList = eurekaClient.getApplication("logger-app").getInstances();
+        InstanceInfo firstInstance = instanceInfoList.get(0);
+        String targetString = firstInstance.getHostName() + ":" + firstInstance.getPort();
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(targetString).usePlaintext().build();
         eventServiceBlockingStub = EventServiceGrpc.newBlockingStub(channel);
     }
 
