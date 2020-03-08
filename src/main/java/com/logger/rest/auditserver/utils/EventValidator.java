9e9796f9004a3ef5a4ab2b75661730e26509b7f4
Jude Niroshan <jude.niroshan11@gmail.com>
2020-03-08 14:44:36 +0100

    validations for basic event fields (#8)

---
+++ src/main/java/com/logger/rest/auditserver/utils/EventValidator.java
@@ -0,0 +1,44 @@
+package com.logger.rest.auditserver.utils;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import com.logger.rest.auditserver.Events;
+import com.logger.rest.auditserver.exception.ValidateException;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class EventValidator {
+
+    public Optional<Events.Event> validate(String jsonString) {
+        List<String> errorList = new ArrayList<>();
+        Events.Event event;
+        try {
+            Events.Event.Builder eventBuilder = Events.Event.newBuilder();
+            JsonFormat.parser().merge(jsonString, eventBuilder);
+            event = eventBuilder.build();
+            if (event.getTimestamp() == 0L) {
+                errorList.add("Mandatory field 'timestamp' is empty");
+            }
+            if (event.getUserId() == 0) {
+                errorList.add("Mandatory field 'userId' is empty");
+            }
+            if (StringUtils.isEmpty(event.getEvent())) {
+                errorList.add("Mandatory field 'event' is empty");
+            }
+        } catch (InvalidProtocolBufferException e) {
+            throw new ValidateException(e.getMessage(), e.getCause());
+        }
+
+        if (!errorList.isEmpty()) {
+            String allErrors = String.join(" and ", errorList);
+            throw new ValidateException(allErrors);
+        }
+
+        return Optional.of(event);
+    }
+}
