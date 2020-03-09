b36f556ba6a96b87d4c3fd53eb6e9ea3c1f31885
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2020-03-09 19:02:07 +0100

    remove unnecessary endpoint

--- src/main/java/com/logger/rest/auditserver/EventController.java
+++ src/main/java/com/logger/rest/auditserver/EventController.java
@@ -1,16 +1,11 @@
 package com.logger.rest.auditserver;
 
 import com.logger.rest.auditserver.utils.EventValidator;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.Instant;
 
 @RestController
 @RequestMapping(path = "/user")
@@ -32,30 +27,5 @@ public class EventController {
                 .map(eventService::handleEvent)
                 .map(EventResponse::new).get();
 
-    }
-
-    @GetMapping(value = "/event")
-    @ResponseBody
-    public boolean test() {
-        // validations needs to be done
-//        throw new ValidationException("no idea");
-        System.out.println("json");
-        Events.Event myEvent = Events.Event.newBuilder().setTimestamp(Instant.now().toEpochMilli())
-                .setUserId(678)
-                .setEvent("some log bro!!!").build();
-        ManagedChannel channel = ManagedChannelBuilder.forTarget("127.0.0.1:50051").usePlaintext()
-                .build();
-        EventServiceGrpc.EventServiceBlockingStub eventServiceBlockingStub = EventServiceGrpc.newBlockingStub(channel);
-        Events.LogStatus logStatus = eventServiceBlockingStub.logToFile(myEvent);
-        System.out.println("logStatus" + logStatus);
-        return true;
-    }
-
-    @GetMapping(value = "/not")
-    @ResponseBody
-    public boolean tester() {
-        System.out.println("json");
-
-        return true;
     }
 }
