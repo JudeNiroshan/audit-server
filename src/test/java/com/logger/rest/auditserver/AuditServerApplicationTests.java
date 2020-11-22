8f350b9a48e4105fb124a89f94779fcbd9d04fb2
jude.niroshan11@gmail.com <jude.niroshan11@gmail.com>
2020-11-22 15:50:31 +0100

    fix test exclusion

--- src/test/java/com/logger/rest/auditserver/AuditServerApplicationTests.java
+++ src/test/java/com/logger/rest/auditserver/AuditServerApplicationTests.java
@@ -3,8 +3,10 @@ package com.logger.rest.auditserver;
 import com.logger.rest.auditserver.config.MyTestConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = {MyTestConfiguration.class})
+@TestPropertySource(locations = "classpath:test-resources.properties")
 class AuditServerApplicationTests {
 
     @Test
